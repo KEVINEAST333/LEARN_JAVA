4529cab630be5a42a4fe1ea9778caa43ad415ef9
KEVINEAST333 <1594066845@qq.com>
2020-01-26 23:28:29 +0800

    frame

--- code1-15/test1.java
+++ code1-15/test1.java
@@ -5,9 +5,9 @@ public class test1 {
 
     public static void main ( String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("璇疯緭鍏ヤ竴涓暟");
+        System.out.print("请输入一个数");
         int a = scanner.nextInt();
-        System.out.printf("%d鐨勫钩鏂规槸%d",a,a*a);
+        System.out.printf("%d的平方是%d",a,a*a);
 
     }
 
