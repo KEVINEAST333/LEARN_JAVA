c51db665369aa46bb7a89f5c6ca7cd782693d1c4
KEVINEAST333 <1594066845@qq.com>
2020-02-25 23:02:22 +0800

    Learn_Java begin

---
+++ code2-25/HelloWord2.java
@@ -0,0 +1,9 @@
+public class HelloWord2 {
+	public static void main(String[] args) {
+		System.out.println("HelloWord2");
+		for(int i = 0;i < args.length;i++) {
+			System.out.print(args[i]+" ");
+		}
+
+	}
+}
