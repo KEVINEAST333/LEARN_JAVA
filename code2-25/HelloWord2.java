7dca698e8d68d110944a7667a1243be0609396fb
KEVINEAST333 <1594066845@qq.com>
2020-02-26 19:02:53 +0800

    text

--- code2-25/HelloWord2.java
+++ code2-25/HelloWord2.java
@@ -1,9 +1,12 @@
 public class HelloWord2 {
 	public static void main(String[] args) {
-		System.out.println("HelloWord2");
+		System.out.println("HelloWord");
+		System.out.print("HelloWord3");
+		System.out.printf("%d",10);
 		for(int i = 0;i < args.length;i++) {
 			System.out.print(args[i]+" ");
 		}
 
 	}
 }
+//输出main方法的args函数内容。
