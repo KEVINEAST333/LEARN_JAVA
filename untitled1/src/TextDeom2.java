105e1ef319c392d99d8edb9ce243a7cbed9426f9
KEVINEAST333 <1594066845@qq.com>
2020-03-01 17:33:00 +0800

    LearnJava

---
+++ untitled1/src/TextDeom2.java
@@ -0,0 +1,31 @@
+import javax.swing.plaf.BorderUIResource;
+
+public class TextDeom2 {
+    public static void main(String[] args) {
+        /*short a = 10;
+        char ch = '王';
+        ch = (char) a;
+        System.out.println(a);
+*/
+       /* int a = 10;
+        long b = 20L;
+        long c = a+b;
+        System.out.println(a+b);
+      */
+     /*  byte a = 10;
+       byte b = 20;
+       int c = a + b;
+       byte d = (byte )(a + b);*/
+     int a = 10;
+     double b = 20.1;
+     a = (int)b ;
+     boolean flg = false;
+     if (false)
+     {
+         System.out.println("123");
+
+     }
+
+
+    }
+}
