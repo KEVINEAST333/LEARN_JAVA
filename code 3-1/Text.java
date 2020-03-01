c840302ef42df8a906a43afd72add9cf1f86bedf
KEVINEAST333 <1594066845@qq.com>
2020-03-01 20:08:13 +0800

    1

---
+++ code 3-1/Text.java
@@ -0,0 +1,13 @@
+public class Text {
+	public static void main(String[] args) {
+		// int car.taxi = 10;
+		short a =128;
+        byte b =(byte) a;
+		 double x=2.0; 
+		 int y=4;
+		 x/=++y; 
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x);
+	}
+}
