188004265bb8808f06fc68706f5e3f1977e53232
KEVINEAST333 <1594066845@qq.com>
2020-03-12 22:53:17 +0800

    text

---
+++ code3-12/src/Text.java
@@ -0,0 +1,7 @@
+
+public class Text {
+    public static void main(String[] args) {
+        int b = 0;
+        System.out.println(b);
+    }
+}
---
+++ untitled2/src/text.java
@@ -0,0 +1,8 @@
+
+public class text {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.println(b);
+    }
+}
