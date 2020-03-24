bccc593027490b8d119aa0d0c0f28986403b4316
KEVINEAST333 <1594066845@qq.com>
2020-03-24 23:40:07 +0800

    SingleLinkedList
    构造单链表
    实现 存储 删除 查找

--- Test/src/SingleLinkedList.java
+++ Test/src/SingleLinkedList.java
@@ -29,9 +29,17 @@ public class SingleLinkedList {
 
     //尾插法
     public void addLast(int data) {
-
+        Node node = new Node(data);
+        if (this.head == null) {
+            this.head = node;
+            return;
+        }
+        Node cur = this.head;
+        while(cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
     }
-
     //任意位置插入,第一个数据节点为0号下标
     public boolean addIndex(int index, int data) {
         return false;
@@ -39,12 +47,33 @@ public class SingleLinkedList {
 
     //查找是否包含关键字key是否在单链表当中
     public boolean contains(int key) {
+        Node cur = this.head;
+        while(cur.next != null) {
+            if(cur.data == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
         return false;
     }
 
     //删除第一次出现关键字为key的节点
     public void remove(int key) {
-
+        Node prve = this.head;
+        Node cur = prve.next;
+        if(prve.data == key) {
+            this.head = prve.next;
+        }
+        else {
+            while (cur != null) {
+                if (cur.data == key) {
+                    prve.next = cur.next;
+                    return;
+                }
+                cur = cur.next;
+                prve = prve.next;
+            }
+        }
     }
 
     //删除所有值为key的节点
@@ -54,7 +83,13 @@ public class SingleLinkedList {
 
     //得到单链表的长度
     public int size() {
-        return -1;
+        int count = 0;
+        Node cur = this.head;
+        while(cur != null) {
+            cur = cur.next;
+            count++;
+        }
+        return count;
     }
 
     public void display() {
--- Test/src/TestDrive.java
+++ Test/src/TestDrive.java
@@ -1,11 +1,20 @@
 public class TestDrive {
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.addFirst(1);
+        /*singleLinkedList.addFirst(1);
         singleLinkedList.addFirst(2);
         singleLinkedList.addFirst(3);
         singleLinkedList.addFirst(4);
-        singleLinkedList.addFirst(5);
+        singleLinkedList.addFirst(5);*/
+        singleLinkedList.addLast(2);
+        singleLinkedList.addLast(2);
+        singleLinkedList.addLast(4);
+        singleLinkedList.addLast(4);
+        singleLinkedList.addLast(5);
+//        singleLinkedList.display();
+//        System.out.println(singleLinkedList.size());
+//        System.out.println(singleLinkedList.contains(20));
+        singleLinkedList.remove(5);
         singleLinkedList.display();
     }
 }
