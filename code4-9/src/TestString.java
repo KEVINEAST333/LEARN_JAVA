b7dd524cad4e47be05ac831aa6782e74e7df47a6
KEVINEAST333 <1594066845@qq.com>
2020-04-09 23:32:20 +0800

    学习String方法

---
+++ code4-9/src/TestString.java
@@ -0,0 +1,20 @@
+public class TestString {
+    public static void main(String[] args) {
+//        String str = "abc,abc.cccc abc,abc.cccc";
+//        String[] result = str.split(" ");
+//        for(String s : result) {
+//            System.out.println(s);
+//        }
+//        System.out.println(str.contains("a"));
+//        String[] result2 = str.split("\\.");
+//        for(String s2 : result2) {
+//            System.out.println(s2);
+//        }
+//        System.out.println(str.contains("a"));
+        String str = "ss";
+        String s = str.replaceAll("s","a");
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+    }
+
