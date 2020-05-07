9af47d856ad944db23cbe16cfec6b4767286df13
KEVINEAST333 <1594066845@qq.com>
2020-05-07 15:19:52 +0800

    Test HeardFrist java

---
+++ code5-1/src/ClassTest/HeadfirstTest.java
@@ -0,0 +1,20 @@
+package ClassTest;
+
+import java.util.ArrayList;
+
+public class HeadfirstTest {
+    public static void main(String[] args) {
+        /*int x = 0;
+        int z = x++;
+        System.out.println(x);
+        System.out.println(z);*/
+        /*String [] array = new String[] {"a","b","c"};
+        for(String i :array) {
+            System.out.println(i);
+        }
+        String S = "123";
+        int a = Integer.parseInt(S);
+        System.out.println(a);*/
+        ArrayList<Integer> array = new ArrayList<>();7
+    }
+}
