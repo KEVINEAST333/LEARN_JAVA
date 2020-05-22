5b1b695805a602a38fd83aad2c38b5c31952a767
KEVINEAST333 <1594066845@qq.com>
2020-05-22 23:56:46 +0800

    练习反射

--- code5-21/src/Cat.java
+++ code5-21/src/Cat.java
@@ -1,8 +1,18 @@
 public class Cat {
-    private String name;
+    private static String name = "小花猫";
     private int age;
-    
+    private static void eat(String food) {
+        System.out.println(name + "吃" + food);
+    }
     public static void main(String[] args) {
         Cat cat = new Cat();
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
--- code5-21/src/TestReflect.java
+++ code5-21/src/TestReflect.java
@@ -1,3 +1,22 @@
-public class TestReflect {
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+public class TestReflect {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        //先获取到类对象
+        Class catClass = Class.forName("Cat");
+        //2. 借助类对象, 获取到指定的 Field 对象
+        // 现在这一步获取到的 field 对象相当于从一个大图纸中获取了一个局部的图纸.
+        Field field = catClass.getDeclaredField("name");
+        //破坏private
+        field.setAccessible(true);
+        Cat cat = new Cat();
+        field.set(cat,"咪咪");
+        System.out.println(cat.toString());
+        //反射方法
+        Method method = catClass.getDeclaredMethod("eat",String.class);
+        method.setAccessible(true);
+        method.invoke(cat,"鱼");
+    }
 }
