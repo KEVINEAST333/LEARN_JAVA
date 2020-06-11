fbe261b270aac295c5735ed16bcd17aa3d0950da
KEVINEAST333 <1594066845@qq.com>
2020-06-11 22:46:59 +0800

    每日一题day10

---
+++ code6-11/src/TestExamDay11.java
@@ -0,0 +1,44 @@
+import com.sun.xml.internal.ws.runtime.config.TubelineFeatureReader;
+
+public class TestExamDay11 {
+       abstract class A {
+
+        }
+    public static void main(String[] args) {
+
+        /*try {
+            int i = 100 / 0;
+            System.out.println(i);
+        }
+        catch (Exception e) {
+            System.out.println(1);
+            throw new RuntimeException();
+        }
+        finally {
+            System.out.println(2);
+        }
+        System.out.println(3);*/
+
+
+        /*int [] array_ = new int[10];
+        System.out.println(array_[10]);*/
+
+        /*byte  b1 = 1, b2 = 2, b3,b6;
+        final byte b4 = 4,b5 = 6;
+        b6 = b4 + b5;
+        //无final修饰 + 后自动转成int
+        b3 = b1 + b2;
+        System.out.println(b3 + b6);*/
+        /*Object o = new Object() {
+            @Override
+            public boolean equals(Object obj) {
+                return true;
+            }
+        };
+        System.out.println(o.equals("sss"));*/
+        String s = "ssss";
+        s = " aaaaa";
+        System.out.println(s);
+
+    }
+}
