5ba2c7530b39ce1127f5a53cc11ae3017cf194db
KEVINEAST333 <1594066845@qq.com>
2020-06-27 23:46:29 +0800

    每日一题day27

---
+++ code6-27/src/Question2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            ArrayList
+        }
+    }
+}
