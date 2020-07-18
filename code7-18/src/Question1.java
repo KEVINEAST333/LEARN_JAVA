e400907b3b11ca455591431d301a89a02e3ee1ba
KEVINEAST333 <1594066845@qq.com>
2020-07-18 23:38:51 +0800

    每日一题day41

---
+++ code7-18/src/Question1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+/*一个数组有N个元素，求连续子数组的最大和。例如: [-1,2,1], 和最大的连续子数组为
+        [2,1]，其和为3*/
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner()
+    }
+}
