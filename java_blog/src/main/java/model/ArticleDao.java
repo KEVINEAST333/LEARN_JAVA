be0e2d1448cf598692089af0c02a195921e44a2f
KEVINEAST333 <1594066845@qq.com>
2020-07-24 14:12:36 +0800

    java_blog ArticleDao2

--- java_blog/src/main/java/model/ArticleDao.java
+++ java_blog/src/main/java/model/ArticleDao.java
@@ -86,6 +86,42 @@ public class ArticleDao {
     }
     // 4. 删除指定文章(给定文章 id 来删除)
     public void delete(int articleId) {
-        
+        //1.连接数据库
+        Connection connection = DBUtil.getConnection();
+        //2.sql语句
+        String sql= "delete from article where articleId = ?";
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,articleId);
+            int ret = statement.executeUpdate();
+            if(ret == 1) {
+                System.out.println("删除成功");
+            } else {
+                System.out.println("删除失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection,statement,null);
+        }
+
+    }
+    public static void main(String[] args) {
+        ArticleDao articleDao = new ArticleDao();
+        // 1. 测试新增文章
+ /*      Article article = new Article();
+        article.setTitle("我是标题2");
+        article.setContent("我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2我是正文2");
+        article.setUseId(1);
+        articleDao.add(article);*/
+        // 2. 测试查看文章列表
+//        List<Article> articles = articleDao.selectAll();
+//        System.out.println(articles);
+        // 3. 查看指定文章内容
+//        Article article = articleDao.selectById(1);
+//        System.out.println(article);
+        // 4. 删除文章
+        articleDao.delete(1);
     }
 }
