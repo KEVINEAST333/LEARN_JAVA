0544abe8619d234728bc3c09ff3f8c6b92dc82bc
kevinwang <dong_w333@163.com>
2021-08-14 14:10:18 +0800

    测试上传github

--- 2021_08_13_testgit/src/Test_Git.java
+++ 2021_08_13_testgit/src/Test_Git.java
@@ -1,5 +1,10 @@
 public class Test_Git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
